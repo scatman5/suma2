c6e2ea541ff4ef0c64fdc4ae564f5ec3c6f3b551
scatman <tomaszjdrysiak@gmail.com>
2022-03-17 22:23:41 +0100

    gf

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let firstNumber = 1;
+let secondNumber = 2;
+
+function dodawania(num1, num2){
+    let suma = num1 + num2;
+
+
+    return suma;
+}
+
+console.log(dodawania(firstNumber, secondNumber))
